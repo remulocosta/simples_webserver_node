3d8fa379ff9477433796eed779f91ae8514b66b9
Remulo Costa <remulo.costa@gmail.com>
2020-03-03 02:03:18 -0300

    move sequece Bio page

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const ip = 'localhost';
 const server = http.createServer((req, res) => {
   const responses = []
   responses['/'] = '<h1>Home</h1>'
+  responses['/bio'] = '<h1>Bio</h1>'
   responses['/inscreva-se'] = '<h1>Inscreva-se</h1>'
-  responses['/bio'] = '<h1>Local</h1>'
   responses['/contato'] = '<h1>Contato</h1>'
   responses['/naoExiste'] = '<h1>URL sem resposta definida!</h1>'
 
