bdd7fb1da5f4f12a1978324dea7814a4c12eb7a4
Remulo Costa <remulo.costa@gmail.com>
2020-03-03 01:58:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   const responses = []
   responses['/'] = '<h1>Home</h1>'
   responses['/inscreva-se'] = '<h1>Inscreva-se</h1>'
-  responses['/local'] = '<h1>Local</h1>'
+  responses['/bio'] = '<h1>Local</h1>'
   responses['/contato'] = '<h1>Contato</h1>'
   responses['/naoExiste'] = '<h1>URL sem resposta definida!</h1>'
 
